0c0660fd2d9e2e334041ec2a1df4b3dcd43b04de
James Mohan <c20305011@mytudublin.ie>
2023-02-24 21:08:41 +0000

    Ability to load scenes

---
+++ SeriousGamesTeamAlpha/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    // Make sure the scene you are trying to load is in the "Scenes in Build" in the build settings :)
+
+    public GameObject loadingScreen;
+    public GameObject activateSceneButton;
+    public Slider slider;
+    public TMP_Text progressText;
+
+    AsyncOperation operation;
+
+    public void LoadLevel (string sceneName)
+    {
+        StartCoroutine(LoadScene(sceneName));
+
+    }
+
+    IEnumerator LoadScene (string sceneName)
+    {
+        loadingScreen.SetActive(true);
+        activateSceneButton.SetActive(false);
+
+        // Here is where you would put the code for a random minigame/ a food pyramid game
+
+        // Loads the scene in the background
+        operation = SceneManager.LoadSceneAsync(sceneName);
+
+        // Don't swap scene until you allow it
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            
+            if (operation.progress >= 0.9f)
+            {
+                // Show the swap scene button when the game is loaded
+                activateSceneButton.SetActive(true);
+            }
+
+            yield return null;
+        }
+    }
+
+    public void ActivateLoadLevel()
+    {
+        // Activate the next scene
+        operation.allowSceneActivation = true;
+        loadingScreen.SetActive(false);
+    }
+}
