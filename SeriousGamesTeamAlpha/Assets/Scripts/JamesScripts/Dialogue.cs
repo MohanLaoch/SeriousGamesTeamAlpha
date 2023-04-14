123ae87f2d16dc88826baffd13389e909f0e881f
James Mohan <c20305011@mytudublin.ie>
2023-04-14 17:16:52 +0100

    added journal without functionality

---
+++ SeriousGamesTeamAlpha/Assets/Scripts/JamesScripts/Dialogue.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+
+    [TextArea(3, 10)]
+    public string[] lines;
+
+    public float textSpeed;
+
+    private int index;
+
+    private void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+    }
+}
