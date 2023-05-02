b36ef13271929880bbf607df9c26b9714dccf376
Linkychu <c20478716@mytudublin.ie>
2023-05-02 23:08:25 +0100

    FINAL PUSH
    AAAA

--- SeriousGamesTeamAlpha/Assets/Scripts/AudioManager/PlaySound.cs
+++ SeriousGamesTeamAlpha/Assets/Scripts/AudioManager/PlaySound.cs
@@ -1,17 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
+    private void Start()
+    {
+        Debug.Log("instantiated");
+    }
+
     public void IconHover()
     {
         AudioManager.instance.Play("IconHover");
+        Debug.Log("sound played icon");
     }
 
     public void ButtonClick()
     {
         AudioManager.instance.Play("ButtonClick");
+        Debug.Log("sound played click");
     }
 
     public void StopRunnerMusic()
